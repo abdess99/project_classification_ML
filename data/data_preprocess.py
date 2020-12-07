fccb02507ac7fe60976bdfb96d7742ebb68890c6
baptistebignaud <68065422+baptistebignaud@users.noreply.github.com>
2020-12-07 23:38:49 +0100

    Update data_preprocess.py

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -21,3 +21,27 @@ def replace_na_values(df : pd.DataFrame) -> pd.DataFrame: #Baptiste
   return df
 
 
+def categoric_to_one_hot(df: pd.DataFrame) -> pd.DataFrame: #Baptiste
+  """
+  Transform categorical data to one hot encoded vectors
+  """
+  n = len(df.columns)
+  cols = [] #Categorical columns we will drop at the end
+  for i in range(n):
+    if df.columns[i].dtype == "string":
+      cols.append(i)
+      df[df.columns[i]] = pd.Categorical(df[df.columns[i]])
+      #One hot encoded version of the categorical column#
+      new_cols = pd.get_dummies(df[df.columns[i]], prefix = list(data.columns)[i]] )
+      df = pd.concat([df, new_cols] , axis=1, sort=False)
+  df.drop(cols)
+  return df
+
+def preprocess_data(file : string) -> pd.DataFrame:
+  df = load_data(file) 
+  df = replace_na_values(df)
+  df = categoric_to_one_hot(df)
+  return df
+
+                      
+  
