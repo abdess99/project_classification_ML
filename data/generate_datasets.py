0a2ac95baec75b3fe19a586782c2536a7de97769
abdess99 <65193616+abdess99@users.noreply.github.com>
2020-12-07 23:12:53 +0100

    Update generate_datasets.py

--- data/generate_datasets.py
+++ data/generate_datasets.py
@@ -1,11 +1,6 @@
-def create_datasets(db, label_class):
+def create_datasets(db):
     data = data_process(db)
-    y= np.array(data[[label_class]])
-    X=data.drop(columns=[label_class])
-    if len(data.columns) - 1 > 4:
-       X_n = StandardScaler().fit_transform(X[1:-1])
-       X_f = np.array(doPcaOnNormalizedData(X_n))
-    else:
-       X_f = np.array(X[1:-1])
-
-return X_f, y
+    y= data.columns[-1]
+    X=data.drop(data.columns[-1])
+    
+return X, y
