832dd35faf11c5bf8cba2b557086bfecc72fd2af
abdess99 <65193616+abdess99@users.noreply.github.com>
2020-12-08 11:42:16 +0100

    Update generate_datasets.py

--- data/generate_datasets.py
+++ data/generate_datasets.py
@@ -1,6 +1,24 @@
-def create_datasets(db):
+import numpy as np
+
+def apply_pca(df: pd.DataFrame) -> pd.DataFrame: #Arthur
+  """
+  Reduce dimensions thanks to pca
+  """
+  pca = decomposition.PCA()
+  df=pca.fit_transform(df)
+  # Selecting only the best components
+  max_variance = np.max(pca.explained_variance_ratio_)
+
+  pc_number = len([v for v in pca.explained_variance_ratio_ if v >= max_variance/10000])
+  df = df[:, :pc_number]
+  return pd.DataFrame(df)
+  
+
+
+def create_datasets(db):    #Arthur
     data=preprocess_data(db)
     y= data[data.columns[-1]]
     X=data.drop(data.columns[-1],axis=1)
-    
+    X=apply_pca(X)
+    X=pd.DataFrame(X)
     return X, y
