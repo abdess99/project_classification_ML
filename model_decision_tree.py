df0f1eb01680d4371adf32c16e8eb9d6b94bde68
abdess99 <65193616+abdess99@users.noreply.github.com>
2020-12-08 03:07:35 +0100

    Update model_decision_tree.py

--- model_decision_tree.py
+++ model_decision_tree.py
@@ -1,33 +1,36 @@
 from sklearn import metrics
 from sklearn import tree
-from data import generate_datasets
+from sklearn.model_selection import KFold
 
 def create_tree(db, criterion, splitter):  #Abdessamad
    X, y = create_datasets(db)
+   
    max = 0
    NUM_SPLITS = 5
    clf = tree.DecisionTreeClassifier(criterion=criterion, splitter=splitter)
    # split the data
    kf = KFold(n_splits=NUM_SPLITS, shuffle=True)
    # Train, test and compute the metrics on each split
+   
    for train_index, test_index in kf.split(X):
-      X_train = X[train_index]
+      
+      X_train = X.iloc[train_index,:]
       y_train = y[train_index]
-      X_test = X[test_index]
+      X_test = X.iloc[test_index,:]
       y_test = y[test_index]
       clf.fit(X_train, y_train)
       # Predictions on the test set
       predictions = clf.predict(X_test)
-      if max < metrics.accuracy_score(test_y, predictions):
-         max = metrics.accuracy_score(test_y, predictions)
-         X_train_f = X[train_index]
+      if max < metrics.accuracy_score(y_test, predictions):
+         max = metrics.accuracy_score(y_test, predictions)
+         X_train_f = X.iloc[train_index,:]
          y_train_f = y[train_index]
-         X_test_f = X[test_index]
+         X_test_f = X.iloc[test_index,:]
          y_test_f = y[test_index]
           
    predictions = clf.predict(X_test_f)
-   clf.fit(train_X_f, train_y_f)
-   recall = metrics.recall_score(test_y_f, predictions)
-   precision=metrics.precision_score(test_y_f, predictions)
+   clf.fit(X_train_f, y_train_f)
+   recall = metrics.recall_score(y_test_f, predictions)
+   precision=metrics.precision_score(y_test_f, predictions)
    
    return clf, precision, recall
