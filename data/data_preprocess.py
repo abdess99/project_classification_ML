bc3c1d73367cbd53b318bb9f9a0b7969330083a1
baptistebignaud <68065422+baptistebignaud@users.noreply.github.com>
2020-12-08 01:44:56 +0100

    Update data_preprocess.py
    Change the way we fill na/values and drop colomuns

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -4,7 +4,7 @@ def load_data(file : str) -> pd.DataFrame:   #Baptiste
   """
   Load the dataset
   """
-  na_val = ["n/a", "na", "?"]
+  na_val = ["n/a", "na", "?", "NaN"]
   df = pd.read_csv(file, na_values = na_val)
   return df
 
@@ -15,13 +15,21 @@ def replace_na_values(df : pd.DataFrame) -> pd.DataFrame: #Baptiste
   n = len(df.columns)
   
   for i in range(n):
-    if df[df.columns[i]].dtype != str and df[df.columns[i]].dtype != object:
-        
+
+    index = 0
+    
+    while type(df[df.columns[i]][index]) == float:
+        print("While",type(df[df.columns[i]][index]))
+        index+=1
+
+    if type(df[df.columns[i]][index])!= str:# and df[df.columns[i]].dtype != object:
         df[df.columns[i]].fillna(df[df.columns[i]].mean())
+        print("If",df[df.columns[i]].dtype)
     
         
     else:
       df[df.columns[i]].fillna(df[df.columns[i]].mode()[0])
+      print("Else",df[df.columns[i]].dtype, type(df[df.columns[i]][1]) )
   df = df.applymap(lambda x: x.replace("\t", "") if type(x) == str else x)
   df = df.applymap(lambda x: x.replace(" ", "") if type(x) == str else x)
  
@@ -34,16 +42,18 @@ def categoric_to_one_hot(df: pd.DataFrame) -> pd.DataFrame: #Baptiste
   n = len(df.columns)
   cols = [] #Categorical columns we will drop at the end
   for i in range(n):
+    #print(df[df.columns[i]].dtype)
     if df[df.columns[i]].dtype == str or df[df.columns[i]].dtype == object:
       cols.append(i)
       df[df.columns[i]] = pd.Categorical(df[df.columns[i]])
       #One hot encoded version of the categorical column#
       new_cols = pd.get_dummies(df[df.columns[i]], prefix = list(df.columns)[i] )
       df = pd.concat([df, new_cols] , axis=1, sort=False)
-  df.drop(cols)
+  df.drop(df.columns[cols], axis=1, inplace=True)
+  print(cols)
   return df
 
-def preprocess_data(file : str) -> pd.DataFrame: #Baptiste
+def preprocess_data(file : str) -> pd.DataFrame:
   df = load_data(file) 
   df = replace_na_values(df)
   df = categoric_to_one_hot(df)
